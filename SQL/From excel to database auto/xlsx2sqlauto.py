560346f1a1acecc5271d340a8db28658dae4a01e
ISoma92 <igricsoma@gmail.com>
2025-02-16 20:28:27 +0100

    add

--- SQL/From excel to database auto/xlsx2sqlauto.py
+++ SQL/From excel to database auto/xlsx2sqlauto.py
@@ -1,9 +1,11 @@
 import openpyxl
-import pyodbc
+import pytds
 
+# Load the workbook and select the active sheet
 wb = openpyxl.load_workbook('MOCK_DATA.xlsx')
 sheet = wb.active
 
+# Create iterators for each column
 iter_a = iter(sheet.iter_rows(min_col=1, max_col=1, min_row=2, values_only=True))
 iter_b = iter(sheet.iter_rows(min_col=2, max_col=2, min_row=2, values_only=True))
 iter_c = iter(sheet.iter_rows(min_col=3, max_col=3, min_row=2, values_only=True))
@@ -13,17 +15,17 @@ iter_f = iter(sheet.iter_rows(min_col=6, max_col=6, min_row=2, values_only=True)
 iter_g = iter(sheet.iter_rows(min_col=7, max_col=7, min_row=2, values_only=True))
 
 # Database connection
-conn = pyodbc.connect(
-    'DRIVER={SQL Server};'
-    'SERVER=DESKTOP-S07K9GM;'
-    'DATABASE=HomeDb;'
-    'Trusted_Connection=yes;'
-    #'UID=your_username;'    Not required if using Windows Authentication on a home SQL Server
-    #'PWD=your_password'     Not required if using Windows Authentication on a home SQL Server
+conn = pytds.connect(
+    server='DESKTOP-S07K9GM',
+    database='HomeDb',
+    user='Szofi',
+   # password='your_password',  # Replace with your actual password
+    auth=pytds.login.SspiAuth(),
+    autocommit=True
 )
 cursor = conn.cursor()
 
-
+# Iterate through the rows and insert data into the database
 for row_a, row_b, row_c, row_d, row_e, row_f, row_g in zip(iter_a, iter_b, iter_c, iter_d, iter_e, iter_f, iter_g):
     if row_a[0] is not None and row_b[0] is not None and row_c[0] is not None and row_d[0] is not None and row_e[0] is not None and row_f[0] is not None and row_g[0] is not None:
         a = str(row_a[0])
@@ -33,9 +35,9 @@ for row_a, row_b, row_c, row_d, row_e, row_f, row_g in zip(iter_a, iter_b, iter_
         e = str(row_e[0])
         f = str(row_f[0])
         g = str(row_g[0])
-        sql = f"INSERT INTO [dbo].[Excel2SQLAuto] (id, first_name, last_name, email, gender, ip_address, country_code) VALUES (?, ?, ?, ?, ?, ?, ?)"
-        cursor.execute(sql, a, b, c, d, e, f, g)
+        sql = f"INSERT INTO [dbo].[Excel2SQLAuto] (id, first_name, last_name, email, gender, ip_address, country_code) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, (a, b, c, d, e, f, g))
 
-conn.commit()
+# Close the connection
 cursor.close()
 conn.close()
