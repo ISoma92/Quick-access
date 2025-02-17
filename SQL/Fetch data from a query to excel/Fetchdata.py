f9690a7dd533c9f3c44347f3b0f828d843564271
ISoma92 <igricsoma@gmail.com>
2025-02-17 10:50:24 +0100

    change to pytds
    odbc is not a valid library for docker usage

--- SQL/Fetch data from a query to excel/Fetchdata.py
+++ SQL/Fetch data from a query to excel/Fetchdata.py
@@ -1,17 +1,16 @@
 import openpyxl
-import pyodbc
+import pytds
 
 wb = openpyxl.load_workbook('Fetched data.xlsx')
 sheet = wb.active
 
 
-conn = pyodbc.connect(
-    'DRIVER={SQL Server};'
-    'SERVER=DESKTOP-S07K9GM;'
-    'DATABASE=HomeDb;'
-    'Trusted_Connection=yes;'
-    #'UID=your_username;'    Not required if using Windows Authentication on a home SQL Server
-    #'PWD=your_password'     Not required if using Windows Authentication on a home SQL Server
+conn = pytds.connect(
+    server='DESKTOP-S07K9GM',
+    database='HomeDb',
+    user='Szofi',
+    auth=pytds.login.SspiAuth(),
+    autocommit=True
 )
 cursor = conn.cursor()
 
